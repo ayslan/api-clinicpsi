fa0d20ec177be9cd2ea39a176cdbb1e68c4ce764
Ayslan Alves <ayslan@analise360.com>
2021-12-06 22:48:40 -0300

    ok

---
+++ Psi.Service/Services/TenantService.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Psi.Domain.Interfaces.Repositories;
+using Psi.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Psi.Service.Services
+{
+    public class TenantService : ITenantService
+    {
+        private readonly IMapper _mapper;
+        private readonly IGlobalUnitOfWork _globalUoW;
+
+        public TenantService(IMapper mapper, IGlobalUnitOfWork globalUoW)
+        {
+            _mapper = mapper;
+            _globalUoW = globalUoW;
+        }
+
+
+    }
+}
