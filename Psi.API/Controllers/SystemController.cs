11c3e33e4d7b09d4c197e3b2e3c3a59a89700612
Ayslan Alves <ayslan@analise360.com>
2022-02-21 14:27:53 -0300

    ok

--- Psi.API/Controllers/SystemController.cs
+++ Psi.API/Controllers/SystemController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Psi.API.Base;
 using Psi.Domain.Interfaces.Services;
 using System;
@@ -18,9 +19,17 @@ namespace Psi.API.Controllers
         }
 
         [HttpGet("cities")]
+        [AllowAnonymous]
         public IActionResult ListCities()
         {
             return Response(_systemService.ListCities());
+        }
+
+        [HttpGet("countries")]
+        [AllowAnonymous]
+        public IActionResult ListCountries()
+        {
+            return Response(_systemService.ListCountries());
         }
     }
 }
